54698c308286128c2fbdc875060ac70afa40fb8c
anasouassou <42014330+anasouassou@users.noreply.github.com>
2026-04-14 01:27:41 +0100

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left, right = 0, len(s)-1
+        while left < right:
+            while left < right and not self.isAscii(s[left]):
+                left += 1
+            while left < right and not self.isAscii(s[right]):
+                right -= 1
+            if s[left].lower() != s[right].lower():
+                return False
+            left, right = left+1, right-1
+        return True
+
+    def isAscii(self, c):
+        return ((ord('a') <= ord(c) <= ord('z')) or
+        (ord('A') <= ord(c) <= ord('Z')) or
+        (ord('0') <= ord(c) <= ord('9'))
+        )
